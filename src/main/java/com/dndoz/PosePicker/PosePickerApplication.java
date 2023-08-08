41854cb10923f400aa496228fdd17d953e0dbe65
leejw-lu <jiwoojenny@naver.com>
2023-08-08 15:41:36 +0900

    chore: 삭제된 파일 복구

---
+++ src/main/java/com/dndoz/PosePicker/PosePickerApplication.java
@@ -0,0 +1,12 @@
+package com.dndoz.PosePicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosePickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosePickerApplication.class, args);
+    }
+}
