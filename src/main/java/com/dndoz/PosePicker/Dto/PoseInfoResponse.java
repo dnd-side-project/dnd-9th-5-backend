7ef60288f5a566fb4bda8acf6ec817d032e477a7
leejw-lu <jiwoojenny@naver.com>
2023-08-10 18:12:59 +0900

    chore: swagger 스미카 이름 변경

--- src/main/java/com/dndoz/PosePicker/Dto/PoseInfoResponse.java
+++ src/main/java/com/dndoz/PosePicker/Dto/PoseInfoResponse.java
@@ -3,7 +3,7 @@ package com.dndoz.PosePicker.Dto;
 import com.dndoz.PosePicker.Domain.PoseInfo;
 import io.swagger.annotations.ApiModel;
 
-@ApiModel(value="포즈 상세 이미지 응답: PoseInfoResponse")
+@ApiModel(value="포즈 이미지 응답: PoseInfoResponse")
 public class PoseInfoResponse {
 
     private PoseInfo poseinfo;
