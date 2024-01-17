5579c05e3555956e426003881fbd28e874ee3c24
olive-su <1466su@gmail.com>
2024-01-17 20:43:51 +0900

    fix: s3 포즈 데이터 업로드 설정

--- src/main/java/com/dndoz/PosePicker/Config/AwsS3Config.java
+++ src/main/java/com/dndoz/PosePicker/Config/AwsS3Config.java
@@ -12,20 +12,20 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
 public class AwsS3Config {
-   @Value("${cloud.aws.credentials.access-key}")
-   private String accessKey;
-   @Value("${cloud.aws.credentials.secret-key}")
-   private String accessSecret;
-   @Value("${cloud.aws.region.static}")
-   private String region;
+	@Value("${cloud.aws.credentials.access-key}")
+	private String accessKey;
+	@Value("${cloud.aws.credentials.secret-key}")
+	private String accessSecret;
+	@Value("${cloud.aws.region.static}")
+	private String region;
 
-   @Bean
-   public AmazonS3 s3Client() {
-       AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
+	@Bean
+	public AmazonS3 s3Client() {
+		AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
 
-       return AmazonS3ClientBuilder.standard()
-               .withCredentials(new AWSStaticCredentialsProvider(credentials))
-               .withRegion(region).build();
-   }
+		return AmazonS3ClientBuilder.standard()
+			.withCredentials(new AWSStaticCredentialsProvider(credentials))
+			.withRegion(region).build();
+	}
 
 }
